e1fd2ed1a2ee62389901015740566131474c25ce
Alisher Zakir <a3zakir@gmail.com>
2019-04-18 16:46:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign (new_obj, object, {[key]: value})
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var new_obj = Object.assign()
+}
